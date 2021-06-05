d388060aac0191655736a56b61956aeef95c9ed3
Md. Nayem Khan <71923841+nayemk1999@users.noreply.github.com>
2021-06-05 22:27:27 +0600

    Delete screens directory

--- screens/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Screen from './Screen';
-
-export const ProfileScreen = ({navigation}) => <Screen navigation ={navigation} name="Profile"/>
-export const MessageScreen = ({navigation}) => <Screen navigation ={navigation} name="Message"/>
-export const ActivityScreen = ({navigation}) => <Screen navigation ={navigation} name="Activity"/>
-export const ListScreen = ({navigation}) => <Screen navigation ={navigation} name="List"/>
